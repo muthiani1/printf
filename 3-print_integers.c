023a19c8446e21cd54ff11d320848ce207620ef1
Muthiani <bluntcoder254@gmail.com>
2024-09-20 15:16:11 +0300

    Handle + ' ' #

--- 3-print_integers.c
+++ 3-print_integers.c
@@ -12,9 +12,9 @@ int print_int(va_list l, flags_t *f)
 	int n = va_arg(l, int);
 	int count = count_digits(n);
 
-	if (f->space == 1 && f->plus == 0 && n > 0)
+	if (f->space == 1 && f->plus == 0 && n >= 0)
 		count += _putchar(' ');
-	if (f->plus == 1 && n > 0)
+	if (f->plus == 1 && n >= 0)
 		count += _putchar('+');
 	if (n <= 0)
 		count++;
